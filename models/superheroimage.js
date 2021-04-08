91c2edd0d41cd53f3f75bc50f36736d3cc1336b8
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-04-08 15:03:16 +0300

    Add basic validation to SuperheroImages model

--- models/superheroimage.js
+++ models/superheroimage.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class SuperheroImage extends Model {
     /**
@@ -9,20 +7,29 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate (models) {
       SuperheroImage.belongsTo(models.Superhero, {
         foreignKey: 'heroId'
       });
     }
-  };
-  SuperheroImage.init({
-    allowNull: false,
-    address: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'SuperheroImage',
-    tableName: 'superhero_images',
-    underscored: true
-  });
+  }
+  SuperheroImage.init(
+    {
+      address: {
+        allowNull: false,
+        type: DataTypes.STRING, 
+        validate: {
+          notNull: true,
+          notEmpty: true
+        }
+      }
+    },
+    {
+      sequelize,
+      modelName: 'SuperheroImage',
+      tableName: 'superhero_images',
+      underscored: true
+    }
+  );
   return SuperheroImage;
 };
