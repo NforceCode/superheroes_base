088bd0f00780867ce22ee3865534f4f4822625fe
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-04-08 09:36:02 +0300

    Add basic error handler

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = require('./routes');
+const errorHander = require('./middlewares/error.handlers');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api', router);
+
+app.use(errorHander);
 
 module.exports = app;
