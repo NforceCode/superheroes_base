e02a8cd319ac4949f487c5cb87362ea4b1182f01
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-04-09 20:39:11 +0300

    Add missing image CRUD

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -1,18 +1,14 @@
-const {Superhero} = require('../models/');
+const { Superhero, SuperheroImage } = require('../models/');
+const createError = require('http-errors');
 
+// добавлять, читать, удалять
 
 module.exports.addHeroImage = async (req, res, next) => {
   try {
     const {
       file: { filename },
-      params: { heroId }
+      hero: superhero
     } = req;
-
-    const superhero = await Superhero.findByPk(heroId);
-
-    if (!superhero) {
-      return next(createError(404));
-    }
 
     const image = await superhero.createSuperheroImage({
       address: filename
@@ -22,7 +18,81 @@ module.exports.addHeroImage = async (req, res, next) => {
       return next(createError(404, 'Error while creating image'));
     }
 
-    res.status(200).send({data: image});
+    res.status(200).send({ data: image });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getImage = async (req, res, next) => {
+  try {
+    const {
+      params: { id }
+    } = req;
+
+    const image = await SuperheroImage.findByPk(id);
+
+    if (!image) {
+      return next(createError(404, 'Image not found'));
+    }
+
+    res.status(200).send({ data: image });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getAllHeroImages = async (req, res, next) => {
+  try {
+    const {
+      params: { heroId }
+    } = req;
+
+    const heroImages = await SuperheroImage.findAll({
+      where: { heroId }
+    });
+
+    if (!heroImages.length) {
+      return next(createError(404, 'Image not found'));
+    }
+
+    res.status(200).send({ data: heroImages });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deleteImage = async (req, res, next) => {
+  try {
+    const {
+      params: { id }
+    } = req;
+
+    const rows = await SuperheroImage.destroy({ where: { id } });
+
+    if (rows !== 1) {
+      return next(createError(404, 'Image not found'));
+    }
+
+    res.status(200).send({ data: 'Image deleted' });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deleteAllHeroImages = async (req, res, next) => {
+  try {
+    const {
+      params: { heroId }
+    } = req;
+
+    const deletedRows = await SuperheroImage.destroy({ where: { heroId } });
+
+    if (deletedRows === 0) {
+      return next(createError(404, 'No images of such hero'));
+    }
+
+    res.status(200).send({ data: 'All images deleted' });
   } catch (err) {
     next(err);
   }
--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -1,10 +1,16 @@
 const imageRouter = require('express').Router();
 const ImagesController = require('../controllers/image.controller');
+const {findHero} = require('../middlewares/superhero.mw');
 const upload = require('../middlewares/multer.mw');
 
-imageRouter.post('/');
-imageRouter.post('/heroes/:heroId',upload.single('image'), ImagesController.addHeroImage);
-imageRouter.post('/heroes/:heroId',upload.single('image'), ImagesController.addHeroImage);
+imageRouter.post('/heroes/:heroId',upload.single('image'), findHero,ImagesController.addHeroImage);
+
+imageRouter.get('/:id', ImagesController.getImage);
+imageRouter.get('/all/:heroId', ImagesController.getAllHeroImages);
+
+imageRouter.delete('/:id', ImagesController.deleteImage);
+imageRouter.delete('/all/:heroId', ImagesController.deleteAllHeroImages);
+
 
 
 module.exports = imageRouter;
