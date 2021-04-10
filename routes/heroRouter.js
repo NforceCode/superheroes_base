a410680913a8f4b49f28eda02460253fa71136da
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-04-10 16:23:39 +0300

    Refactor superhero routing

--- routes/heroRouter.js
+++ routes/heroRouter.js
@@ -2,16 +2,14 @@ const {Router}= require('express');
 const HeroController = require('../controllers/superhero.controller');
 const pagination = require('../middlewares/pagination.mw');
 const upload = require('../middlewares/multer.mw');
+const imageRouter = require('./imageRouter');
 const heroRouter = Router();
 
-heroRouter.post('/', HeroController.createSuperhero);
+heroRouter.post('/', upload.array('images', 10), HeroController.createSuperhero);
 heroRouter.get('/', pagination, HeroController.getSuperheroes);
 heroRouter.get('/:id', HeroController.getSuperhero);
-heroRouter.patch('/:id', HeroController.updateSuperhero);
+heroRouter.patch('/:id',upload.array('images', 10), HeroController.updateSuperhero);
 heroRouter.delete('/:id', HeroController.deleteSuperhero);
 
-heroRouter.post('/withimage/',upload.single('image'), HeroController.addHeroWithImage);
-heroRouter.post('/withimages/',upload.array('images', 5), HeroController.addHeroWithImages);
-heroRouter.post('/withall/',upload.array('images', 10), HeroController.createWithAll);
-
+heroRouter.use('/:heroId/images', imageRouter);
 module.exports = heroRouter;
